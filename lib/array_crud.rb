9425c0e804917aa95b5aae605a37b6c9308d3c5d
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 17:14:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  instruments = ["guitar", "bass", "piano", "drums"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(instruments, element)
+ instruments << "arrays!"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(instruments, element)
+  instruments.unshift("wow")
+ 
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(instruments)
+  instruments.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(instruments)
+  instruments.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(instruments, index_number)
+  instruments[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(instruments)  
+  instruments[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(instruments)
+  instruments[-1]
+
 end
