936e49aff621a2b54ca1802459d83c8810f90da8
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 16:34:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  instruments = ["guitar", "bass", "piano", "drums"]
-  instruments.push("violin")
+  instruments 
 end
 
 def add_element_to_start_of_array(array, element)
