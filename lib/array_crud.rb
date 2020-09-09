1d071435918b5214f44ba89bee0a92534872542a
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 16:44:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 
 end
 
-def retrieve_element_from_index(instruments)
-  instruments[2]
+def retrieve_element_from_index(array, index_number)
 
 end
 
