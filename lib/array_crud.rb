e6726a9f05b08f638672c1ee54b1ef4a43a9dfcf
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 16:35:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,29 +8,28 @@ end
 
 def add_element_to_end_of_array(array, element)
  instruments = ["guitar", "bass", "piano", "drums"]
-  instruments <<
+  instruments << "violin"
 end
 
 def add_element_to_start_of_array(array, element)
-  instruments.unshift("violin")
+ 
 end
 
 def remove_element_from_end_of_array(array)
-  instruments.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  instruments.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  instruments[2]
+
 end
 
-def retrieve_first_element_from_array(array)
-  instruments[0]
+def retrieve_f  instruments[0]
 end
 
 def retrieve_last_element_from_array(array)
-  instruments[-1]
+
 end
