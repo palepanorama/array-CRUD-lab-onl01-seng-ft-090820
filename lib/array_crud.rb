0360a6c895aa9b029b3826e38bd3bc1d4706c5f5
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 17:10:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(instruments, violin)
- instruments << "violin"
+ instruments << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
